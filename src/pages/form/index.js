940571e32a49d7d9608b3c68fffe9f58eb8ed1f1
ouyangdc <ouyangdc@chinawyny.com>
2018-11-19 10:18:58 +0800

    封装redux的connect写法

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,16 +1,46 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import reduxHelper from '../../utils/reduxHelper'
-import { connect } from '@tarojs/redux'
+import inject from '../../utils/inject'
 import './index.less'
 import { extend } from 'nerv-utils';
 
-const mapStateToProps = (store) => {
-    const { count } = store
-    if (count && JSON.stringify(count) !== '{}') {
-        return {
-            count: count.value
-        }
+@inject('count')
+export default class Form extends Component {
+
+    config = {
+        navigationBarTitleText: '表单页'
+    }
+    state = {
+        step: 1
+    }
+    componentDidMount() {
+        reduxHelper('count', {value: 1})
+    }
+    preStep = () => {
+        this.state.step === 1 ?
+            Taro.redirectTo({ url: 'pages/index/index' }) :
+            this.setState({ step: this.state.step - 1 });
+            reduxHelper('count', { value: this.props.count.value - 1 })
+    }
+    nextStep = () => {
+        this.state.step === 3 ?
+            Taro.redirectTo({ url: 'pages/result/index' }) :
+            this.setState({ step: this.state.step + 1 });
+            reduxHelper('count', { value: this.props.count.value + 1 })
+    }
+    render() {
+        const { edition } = this.$router.params
+        return (
+            <View className='form'>
+                <h3>{edition}版</h3>
+                <h3 className="title">第{this.state.step}步</h3>
+                <Content step={this.state.step} />
+                <Button onClick={this.preStep}>上一步</Button>
+                <Button onClick={this.nextStep}>下一步</Button>
+                <div>{this.props.count.value}</div>
+            </View>
+        )
     }
 }
 
@@ -35,36 +65,3 @@ class Content extends Component {
         )
     }
 }
-@connect(mapStateToProps)
-export default class Form extends Component {
-
-    config = {
-        navigationBarTitleText: '表单页'
-    }
-    state = {
-        step: 1
-    }
-    preStep = () => {
-        this.state.step === 1 ?
-            Taro.redirectTo({ url: 'pages/index/index' }) :
-            this.setState({ step: this.state.step - 1 });
-    }
-    nextStep = () => {
-        this.state.step === 3 ?
-            Taro.redirectTo({ url: 'pages/result/index' }) :
-            this.setState({ step: this.state.step + 1 });
-    }
-    render() {
-        const { edition } = this.$router.params
-        return (
-            <View className='form'>
-                <h3>{edition}版</h3>
-                <h3 className="title">第{this.state.step}步</h3>
-                <Content step={this.state.step} />
-                <Button onClick={this.preStep}>上一步</Button>
-                <Button onClick={this.nextStep}>下一步</Button>
-            </View>
-        )
-    }
-}
-
