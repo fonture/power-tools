a235d4c2a9c40abab523ac536ced33a052068a8e
ouyangdc <ouyangdc@chinawyny.com>
2018-11-19 16:26:12 +0800

    步骤条提升到form的顶级组件中

--- src/pages/form/Step2/index.js
+++ src/pages/form/Step2/index.js
@@ -1,17 +1,11 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import Steps from '../../../components/steps'
 
 export default class Step2 extends Component {
     render() {
-        const items = [
-            '第一步',
-            '第二步',
-            '第三步'
-        ]
         return (
             <View>
-                <Steps current={2} items={items}/>
+                第二步
             </View>
         )
     }
--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -2,10 +2,12 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import reduxHelper from '../../utils/reduxHelper'
 import inject from '../../utils/inject'
+import Steps from '../../components/steps'
+
 import './index.less'
 import { extend } from 'nerv-utils';
 
-@inject('count')
+@inject('stepInfo')
 export default class Form extends Component {
 
     config = {
@@ -15,30 +17,29 @@ export default class Form extends Component {
         step: 1
     }
     componentDidMount() {
-        reduxHelper('count', {value: 1})
+        reduxHelper('stepInfo', {current: 0, items: ['基础信息', '用电成本','购电计算']})
     }
     preStep = () => {
         this.state.step === 1 ?
             Taro.redirectTo({ url: 'pages/index/index' }) :
             this.setState({ step: this.state.step - 1 });
-            reduxHelper('count', { value: this.props.count.value - 1 })
+        reduxHelper('stepInfo', {current: this.state.step - 2, items: ['基础信息', '用电成本','购电计算']})
     }
     nextStep = () => {
         this.state.step === 3 ?
             Taro.redirectTo({ url: 'pages/result/index' }) :
             this.setState({ step: this.state.step + 1 });
-            reduxHelper('count', { value: this.props.count.value + 1 })
+        reduxHelper('stepInfo', {current: this.state.step, items: ['基础信息', '用电成本','购电计算']})
     }
     render() {
         const { edition } = this.$router.params
+        const { stepInfo } = this.props
         return (
             <View className='form page'>
-                <h3>{edition}版</h3>
-                <h3 className="title">第{this.state.step}步</h3>
+                <Steps current={stepInfo.current} items={stepInfo.items}/>
                 <Content step={this.state.step} />
                 <Button onClick={this.preStep}>上一步</Button>
                 <Button onClick={this.nextStep}>下一步</Button>
-                <div>{this.props.count.value}</div>
             </View>
         )
     }
