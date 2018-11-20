5d96cc56409da0b86f255cfc26595571202a7441
ouyangdc <ouyangdc@chinawyny.com>
2018-11-20 16:05:16 +0800

    新增用电成本功能

--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -3,10 +3,10 @@
  * @Date: 2018-11-19 11:03:54 
  * @Description: 表单顶部步骤条
  * @Last Modified by: ouyangdc
- * @Last Modified time: 2018-11-19 16:24:47
+ * @Last Modified time: 2018-11-20 10:00:09
  */
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Input } from '@tarojs/components'
 import './index.less'
 
 export default class Steps extends Component {
@@ -17,14 +17,14 @@ export default class Steps extends Component {
             {
                 items.map((item, index) => {
                     return [
-                        <View className={index === current ? 'flex-item current' : 'flex-item'} >
-                            <div className="checkbox-container" style={{borderColor: index < current ? '#636465' : '#D8E4E4'}}>
-                                <div className={index <= current ? 'checked' : ''}>
-                                    {
-                                        index < current ? '✓' : ''
-                                    }
-                                </div>
-                            </div>
+                        <View className="flex-item">
+                            <Input 
+                                type="checkbox" 
+                                className={index === current ? 'current' : ''} 
+                                checked={index <= current ? true : false}
+                                style={{borderColor: index < current ? '#636465' : '#D8E4E4'}} 
+                                disabled
+                            />
                             <Text style={{color: index < current ? '#636465' : '#D8E4E4', fontSize: '10px'}}>{item}</Text>
                         </View>,
                         index === items.length - 1 ? null : <hr style={{borderColor: index < current ? '#636465' : '#EDFAFA'}}/>
