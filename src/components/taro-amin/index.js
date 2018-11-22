c885258cbee74c23aaaf86e2eb8212b51fc10200
jinxin <m18508218948@163.com>
2018-11-22 17:03:03 +0800

    更新过场动画bug

--- src/components/taro-amin/index.js
+++ src/components/taro-amin/index.js
@@ -3,10 +3,13 @@
 export default (Comp, props) => {
     let {action} = props;
     const didMount = (dom)=>{
-        // dom.className += ` slide-in-${action}`;
-        // setTimeout(()=>{
-        //     dom.className += ` slide-in-${action}-active`;
-        // },0)
+        dom.className += ` slide-in-${action}`;
+        process.nextTick(()=>{
+            dom.className += ` slide-in-${action}-active`;
+        })
+        setTimeout(()=>{
+            dom.className = '';
+        },200)
     }
     return <Comp {...props} didMount={didMount}/>
 };
