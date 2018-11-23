93eaca492290fbce27fb4b79bc50a070e41b08c3
tianl <tianl@chinawyny.com>
2018-11-23 16:49:15 +0800

    一半的计算

--- src/utils/formula.js
+++ src/utils/formula.js
@@ -1,5 +1,30 @@
 export function test() {
     
 }
+// 未参与市场
+/**
+   * @description 未参与市场，用电
+   */
+export class noMart{
+  /**
+   * @description 未参与市场 基金计算  对象包含值
+   * @param {string} prce 平水期峰段目录电价
+   * @param {string} scale
+   * 
+   * 
+   * 
+   */
+  buyCost = (args) =>{
+    const {} = args;
+  }
+}
+// 参与市场
+export class haveMart {
+  /**
+   * @description 未参与市场 基金计算  对象包含值
+   * @param {string} prce 平水期峰段目录电价
+   */
+  buyCost = () =>{
 
-// export function 
+  }
+}
