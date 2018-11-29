44b5ccd473bc03224a2971de034398a60b28d78c
🔥 TommY ❄️ <lihroff@icloud.com>
2018-11-29 14:32:00 +0800

    delete file

--- src/pages/higher/Step3/index.js
+++ src/pages/higher/Step3/index.js
@@ -1,18 +1,17 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import UnPartake from './UnPartake'
-import Partake from './Partake'
 import './index.less'
 
 export default class Step3 extends Component {
-    componentDidMount() {
-        this.props.onDidMount(this._rendered.dom);
-    }    
-    render() {
-        return (
-            <View>
-                <Partake />
-            </View>
-        )
-    }
+  componentDidMount() {
+    console.log(this);
+    this.props.onDidMount(this._rendered.dom);
+  }
+  render() {
+    return (
+      <View>
+        todo todo todo
+      </View>
+    )
+  }
 }
